685d447e0488b1316ec490c4608cf2e15fa00ef5
Rob Bailiff <robbailiff@gmail.com>
2020-11-28 22:45:51 +0000

    Major changes to HTML, CSS, JS and README files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rock-paper-scissors
-A simple implementation of Rock, Paper, Scissors game
+# Rock Paper Scissors Game
+
+## Introduction
+
+For this mini-project, the assignment was to create a Rock Paper Scissors game using Javascript to practice what we've learnt about control flow, loops and DOM interaction. The game should allow the user to make their choice via clicking a button, generate a random choice for the computer player, display the running score, and announce a winner of the game once one player reaches 5 points.
+
+Check out my attempt: https://robbailiff.github.io/rock-paper-scissors/
+
+The assignment is part of **The Odin Project** curriculum, which is free and open source and uses on freely available resources in each of the modules. The ethos of the project is learning by building things, with the added bonus of creating projects that can be showcased in a portfolio.
+
+Check out The Odin Project [here](https://www.theodinproject.com/).
+
+## Learning
+This was quite a challenging but also very enjoyable project. There was an awful lot to take in from the DOM Manipulation lesson so it took some time to sit down and think about the best way to approach the problem. It didn't take me long to all to code out the basic logic but it did tae a bit of time to figure out the best way to organise the button logic. This did force me to think "in Javascript" because I had originally tried to embed the event listeners inside a while loop, as that is what I would do if I was making a similar game in Python. I then realised that wouldn't be necessary when I could instead simply embed the game functionality in the event listener callback. I spent about a day or so thinking about the project and then spent probably 10-15 hours working on it and refactoring the code.
+
+I particularly enjoyed doing all the CSS styling and probably about 40% of the time spent on the project was spent on the visual design. The theme was inspired by 80s sci-fi movie computer terminals like those in **Aliens** film franchise, and also by the Pip-boy computer from the **Fallout** video game series. I learnt a lot about animations and their properties whilst creating the loading screen and the flicker effect, which were a lot of fun. Overall this was a great project from the course designers at The Odin Project that really reinforced the concepts that have been taugh so far.
--- game.js
+++ game.js
@@ -3,7 +3,11 @@
 let playerScore = 0;
 let computerScore = 0;
 
-// #######################################################################
+const displayChoice = document.querySelector(".display-choice");
+const displayResult = document.querySelector(".display-result");
+
+const playerScoreboard = document.querySelector("div.player-score h2");
+const computerScoreboard = document.querySelector("div.computer-score h2");
 
 
 const buttons = document.querySelectorAll('button.player-choice');
@@ -15,19 +19,34 @@ const buttons = document.querySelectorAll('button.player-choice');
                 let result = playRound(playerChoice, computerChoice);
                 if (result === "Win") {
                     playerScore++;
+                    playerScoreboard.textContent = playerScore;
                 } else if (result === "Lose") {
                     computerScore++;
+                    computerScoreboard.textContent = computerScore;
                 }
 
                 if (playerScore === 5) {
-                    console.log(`You win ${playerScore}:${computerScore}`);
+                    displayChoice.textContent = "Congratulations!";
+                    displayResult.textContent = "You have won the match!";
+                    buttons.forEach((button) => {
+                        button.disabled = true;
+                    });
                 } else if (computerScore === 5) {
-                    console.log(`You lose ${computerScore}:${playerScore}`);
+                    displayChoice.textContent = "Better luck next time!";
+                    displayResult.textContent = "You have lost the match!";
+                    buttons.forEach((button) => {
+                        button.disabled = true;
+                    });
                 }
             });
         });
 
-// #######################################################################
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", () => {
+    resetGame();
+})
+
 
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
@@ -36,91 +55,57 @@ function computerPlay() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    const display = document.querySelector(".display")
-    display.textContent = `You selected ${playerChoice} and the computer selected ${computerChoice}.`;
+
+    displayChoice.textContent = `You selected ${playerChoice} and the computer selected ${computerChoice}.`;
     
     let result;
     
     switch (playerChoice) {
         case 'rock':
             if (computerChoice === 'paper') {
-                console.log("You lose! Paper beats rock!")
+                displayResult.textContent = "You lose! Paper beats rock!";
                 result = "Lose";
                 break;
             } else if (computerChoice === 'scissors') {
-                console.log("You win! Rock beats scissors!")
+                displayResult.textContent = "You win! Rock beats scissors!";
                 result = "Win";
                 break;
             }
         case 'paper':
             if (computerChoice === 'rock') {
-                console.log("You win! Paper beats rock!")
+                displayResult.textContent = "You win! Paper beats rock!";
                 result = "Win";
                 break;
             } else if (computerChoice === 'scissors') {
-                console.log("You lose! Scissors beats paper!");
+                displayResult.textContent = "You lose! Scissors beats paper!";
                 result = "Lose"
                 break;
             }
         case 'scissors':
             if (computerChoice === 'rock') {
-                console.log("You lose! Rock beats scissors!");
+                displayResult.textContent = "You lose! Rock beats scissors!";
                 result = "Lose"
                 break;
             } else if (computerChoice === 'paper') {
-                console.log("You win! Scissors beats paper!")
+                displayResult.textContent = "You win! Scissors beats paper!";
                 result = "Win";
                 break;
             }
         default:
-            console.log("It's a draw!");
+            displayResult.textContent = "It's a draw!";
             result = "Draw"
     }
     return result;
 }
 
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     // while (playerScore < 5 || computerScore < 5) {
-//     for (let i = 0; i <= 5; i++) {
-        
-//         let playerChoice;
-//         const buttons = document.querySelectorAll('button.player-choice');
-//         buttons.forEach((button) => {
-//             button.addEventListener("click", (e) => {
-//                 e.stopPropagation;
-//                 let playerChoice = e.target.id;
-//                 let computerChoice = computerPlay();
-//                 let result = playRound(playerChoice, computerChoice);
-//                 if (result === "Win") {
-//                     playerScore++;
-//                 } else if (result === "Lose") {
-//                     computerScore++;
-//                 }
-//                 console.log(e.target.id);
-//                 console.log(`Inside variable value: ${playerChoice}`);
-//             });
-//         });
-
-       
-
-//         let playerChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
-//         let computerChoice = computerPlay();
-//         let result = playRound(playerChoice, computerChoice);
-//         if (result === "Win") {
-//             playerScore++;
-//         } else if (result === "Lose") {
-//             computerScore++;
-//         }
-//     }
-//     if (playerScore > computerScore) {
-//         console.log(`You win ${playerScore}:${computerScore}`);
-//     } else if (computerScore > playerScore) {
-//         console.log(`You lose ${computerScore}:${playerScore}`);
-//     } else {
-//         console.log(`You draw ${playerScore}:${computerScore}`);
-//     }
-// }
-
-// game();
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    displayChoice.textContent = "Rock, Paper, Scissors?";
+    displayResult.textContent = "Make your choice!";
+    playerScoreboard.textContent = 0;
+    computerScoreboard.textContent = 0;
+    buttons.forEach((button) => {
+        button.disabled = false;
+    });
+}
